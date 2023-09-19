994a0b4b3d9d7b02250f4d2b50c04e6a2ed20383
Raed Majeed <raedam786@gmail.com>
2023-09-19 15:36:48 +0530

    added circuit breaker resilience4j

--- order-service/src/main/java/com/raedmajeed/orderservice/Controller/OrderController.java
+++ order-service/src/main/java/com/raedmajeed/orderservice/Controller/OrderController.java
@@ -2,10 +2,15 @@ package com.raedmajeed.orderservice.Controller;
 
 import com.raedmajeed.orderservice.DTO.OrderRequest;
 import com.raedmajeed.orderservice.Service.OrderServiceImpl;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @Tag(
@@ -19,7 +24,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createOrder(@RequestBody OrderRequest orderRequest) {
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> createOrder(@RequestBody OrderRequest orderRequest) {
         orderService.createOrder(orderRequest);
+        return CompletableFuture.supplyAsync(() -> "Order placed Successfully");
+    }
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() -> "OOPS something went wrong, place order after some time");
     }
 }
